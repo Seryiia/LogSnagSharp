3c337ab58cc49b99822a31c6d825daae2ffc5434
Seryiia <seryiiarinaka@gmail.com>
2022-03-16 15:02:16 -0400

    Initial commit

---
+++ LogSnag/LogSnag.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace LogSnag
+{
+    public class LogSnag
+    {
+        private const String LOGSNAG_URL = "https://api.logsnag.com/v1/log";
+        private String AuthorizationToken { get; }
+        private String? Project { get; set; }
+        
+        public LogSnag(String authToken)
+        {
+            AuthorizationToken = authToken;
+        }
+
+        public LogSnag(String authToken, String project)
+        {
+            AuthorizationToken = authToken;
+            Project = project;
+        }
+
+        public void SetProject(String project)
+        {
+            Project = project;
+        }
+
+        public void Publish(String channel, String eventName, String? description = null, String? icon = null, bool? sendPushNotif = null)
+        {
+            if(String.IsNullOrEmpty(Project))
+            {
+                throw new InvalidOperationException("Project must be set using SetProject() before calling Publish()"); 
+            }
+
+            // Use configured project
+            PublishToProject(Project, channel, eventName, description, icon, sendPushNotif);
+        }
+
+        public void PublishToProject(String project, String channel, String eventName, String? description = null, String? icon = null, bool? sendPushNotif = null)
+        {
+            // Validate
+            if (String.IsNullOrEmpty(project) || String.IsNullOrEmpty(channel) || String.IsNullOrEmpty(eventName))
+            {
+                throw new InvalidOperationException("Publish attempted with an empty project, channel, or event name.");
+            }
+
+            // Construct the request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(LOGSNAG_URL);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.Headers["Authorization"] = $"Bearer {AuthorizationToken}";
+
+            // Construct and send the request
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                // Don't write null values
+                JsonSerializerOptions options = new()
+                {
+                    DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+                };
+
+                // Serialize payload
+                String json = JsonSerializer.Serialize(new
+                {
+                    project = project,
+                    channel = channel,
+                    @event = eventName, // Language keywords must be prefixed with @ to be used as a variable name
+                    description = description,
+                    icon = icon,
+                    notify = sendPushNotif
+                }, options);
+
+                streamWriter.Write(json);
+            }
+
+            // Verify response processes properly
+            HttpWebResponse httpResponse = (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
---
+++ LogSnagTest/LogSnagTest.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace LogSnagTest
+{
+    [TestClass]
+    public class LogSnagTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(WebException))]
+        public void InvalidAuthIdThrowsWebException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("invalid", "c-sharp");
+            client.Publish("test", "InvalidAuthIdThrowsWebException Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(WebException))]
+        public void InvalidProjectThrowsWebException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c", "invalid");
+            client.Publish("test", "NotSettingProjectOptionsFails Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NotSettingProjectThrowsInvalidOperationException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.Publish("test", "NotSettingProjectOptionsFails Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyProjectThrowsInvalidOperationException()
+        {LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c", "");
+            client.Publish("test", "EmptyProjectThrowsInvalidOperationException Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyChannelThrowsInvalidOperationException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c", "c-sharp");
+            client.Publish("", "EmptyChannelThrowsInvalidOperationException Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyEventNameThrowsInvalidOperationException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c", "c-sharp");
+            client.Publish("test", "");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PublishToProjectEmptyProjectThrowsInvalidOperationException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("", "test", "PublishToProjectEmptyProjectThrowsInvalidOperationException Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PublishToProjectEmptyChannelThrowsInvalidOperationException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("c-sharp", "", "PublishToProjectEmptyChannelThrowsInvalidOperationException Test Event");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PublishToProjectEmptyEventNameThrowsInvalidOperationException()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("c-sharp", "test", "");
+        }
+
+        [TestMethod]
+        public void EventOnlyPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.SetProject("c-sharp");
+            client.Publish("test", "EventOnlyPublishTest Event");
+        }
+
+        [TestMethod]
+        public void EventDescriptionPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.SetProject("c-sharp");
+            client.Publish("test", "EventDescriptionPublishTest Event", "A test testing description publishing");
+        }
+
+        [TestMethod]
+        public void EventDescriptionIconPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.SetProject("c-sharp");
+            client.Publish("test", "EventDescriptionIconPublishTest Event", "A test testing icon publishing", "⌛");
+        }
+
+        [TestMethod]
+        public void EventDescriptionIconPushNotifPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.SetProject("c-sharp");
+            client.Publish("test", "EventDescriptionIconPublishTest Event", "A test testing push notif publishing", "⌛", true);
+        }
+
+        [TestMethod]
+        public void PublishToProjectEventOnlyPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("c-sharp", "test", "ExplictEventOnlyPublishTest Event");
+        }
+
+        [TestMethod]
+        public void PublishToProjectEventDescriptionPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("c-sharp", "test", "ExplicitEventDescriptionPublishTest Event", "A test testing description publishing");
+        }
+
+        [TestMethod]
+        public void PublishToProjectEventDescriptionIconPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("c-sharp", "test", "ExplictEventDescriptionIconPublishTest Event", "A test testing icon publishing", "⌛");
+        }
+
+        [TestMethod]
+        public void PublishToProjectEventDescriptionIconPushNotifPublishTest()
+        {
+            LogSnag.LogSnag client = new LogSnag.LogSnag("20b24bdc32917e35e38b58f5e7d6019c");
+            client.PublishToProject("c-sharp", "test", "ExplictEventDescriptionIconPushNotifPublishTest Event", "A test testing push notif publishing", "⌛", true);
+        }
+    }
+}
